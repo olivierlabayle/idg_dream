81e60359d2dc1c533d8fdf4f7b68cf8f61c5429f
Olivier Labayle <olabayle@gmail.com>
2019-03-10 21:24:34 +0100

    add inchi loader

--- idg_dream/transforms.py
+++ idg_dream/transforms.py
@@ -6,8 +6,9 @@ class SequenceLoader:
     def __init__(self, engine):
         self.engine = engine
 
-    def build_query(self, uniprot_ids):
-        return text(f"""SELECT uniprot_id, sequence 
+    @staticmethod
+    def build_query(uniprot_ids):
+        return text(f"""SELECT uniprot_id AS target_id, sequence 
                             FROM uniprot_proteins 
                             INNER JOIN 
                                 (SELECT unnest(ARRAY{uniprot_ids}) AS uniprot_id) query_ids
@@ -16,7 +17,25 @@ class SequenceLoader:
     def transform(self, X):
         query = self.build_query(X['target_id'].unique().tolist())
         sequences = pd.read_sql(query, self.engine)
-        return X.merge(sequences, how="inner", left_on="target_id", right_on="uniprot_id")
+        return X.merge(sequences, how="inner", on="target_id")
+
+
+class InchiLoader:
+    def __init__(self, engine):
+        self.engine = engine
+
+    @staticmethod
+    def build_query(inchi_keys):
+        return text(f"""SELECT DISTINCT standard_inchi_key, standard_inchi FROM
+                            compound_structures 
+                            INNER JOIN 
+                                (SELECT unnest(ARRAY{inchi_keys}) AS standard_inchi_key) query_compounds
+                            USING(standard_inchi_key);""")
+
+    def transform(self, X):
+        query = self.build_query(X["standard_inchi_key"].unique().tolist())
+        inchis = pd.read_sql(query, self.engine)
+        return X.merge(inchis, how="inner", on="standard_inchi_key")
 
 
 class ColumnFilter:
--- tests/tests_idg_dream/tests_transformers.py
+++ tests/tests_idg_dream/tests_transformers.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 from idg_dream.settings.test import DB_PORT
 
-from idg_dream.transforms import SequenceLoader, ColumnFilter
+from idg_dream.transforms import SequenceLoader, ColumnFilter, InchiLoader
 
 
 class TestSequenceLoader(unittest.TestCase):
@@ -21,7 +21,8 @@ class TestSequenceLoader(unittest.TestCase):
     def tearDownClass(cls):
         cls.engine.execute("DROP TABLE IF EXISTS uniprot_proteins;")
 
-    def get_X(self):
+    @staticmethod
+    def get_X():
         return pd.DataFrame([['Q9Y4K4', 0],
                              ['Q9Y478', 1],
                              ['Q9Y4K4', 2],
@@ -31,11 +32,53 @@ class TestSequenceLoader(unittest.TestCase):
 
     def test_transform(self):
         X_transformed = self.transformer.transform(self.get_X())
-        expected_df = pd.DataFrame([['Q9Y4K4', 0, 'Q9Y4K4', 'ACGTG'],
-                                    ['Q9Y478', 1, 'Q9Y478', 'GTGTGG'],
-                                    ['Q9Y4K4', 2, 'Q9Y4K4', 'ACGTG'],
-                                    ['Q9UL54', 3, 'Q9UL54', 'CCTAGTAA']],
-                                   columns=['target_id', 'index_column', 'uniprot_id', 'sequence'])
+        expected_df = pd.DataFrame([['Q9Y4K4', 0, 'ACGTG'],
+                                    ['Q9Y478', 1, 'GTGTGG'],
+                                    ['Q9Y4K4', 2, 'ACGTG'],
+                                    ['Q9UL54', 3, 'CCTAGTAA']],
+                                   columns=['target_id', 'index_column', 'sequence'])
+        X_transformed = X_transformed.sort_values('index_column')
+        X_transformed.index = list(range(len(X_transformed)))
+        pd.testing.assert_frame_equal(X_transformed, expected_df)
+
+
+class TestInchiLoader(unittest.TestCase):
+    engine = create_engine(f'postgresql+pg8000://idg_dream@127.0.0.1:{DB_PORT}/idg_dream', echo=False)
+    transformer = InchiLoader(engine)
+
+    @classmethod
+    def setUpClass(cls):
+        uniprot_data = pd.DataFrame([['OWRSAHYFSSNENM-UHFFFAOYSA-N',
+                                      'InChI=1S/C17H12ClN3O3/c1-10-8-11(21-17(24)20-15(22)9-19-21)6-7-12(10)16(23)13-4-2-3-5-14(13)18/h2-9H,1H3,(H,20,22,24)'],
+                                     ['ZJYUMURGSZQFMH-UHFFFAOYSA-N',
+                                      'InChI=1S/C18H12N4O3/c1-11-8-14(22-18(25)21-16(23)10-20-22)6-7-15(11)17(24)13-4-2-12(9-19)3-5-13/h2-8,10H,1H3,(H,21,23,25)'],
+                                     ['YOMWDCALSDWFSV-UHFFFAOYSA-N',
+                                      'InChI=1S/C18H16ClN3O3/c1-10-7-14(22-18(25)21-15(23)9-20-22)8-11(2)16(10)17(24)12-3-5-13(19)6-4-12/h3-9,17,24H,1-2H3,(H,21,23,25)']],
+                                    columns=["standard_inchi_key", 'standard_inchi'])
+        uniprot_data.to_sql('compound_structures', cls.engine, if_exists='replace')
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.engine.execute("DROP TABLE IF EXISTS compound_structures;")
+
+    @staticmethod
+    def get_X():
+        return pd.DataFrame([['OWRSAHYFSSNENM-UHFFFAOYSA-N', 0],
+                             ['YOMWDCALSDWFSV-UHFFFAOYSA-N', 1],
+                             ['OWRSAHYFSSNENM-UHFFFAOYSA-N', 2],
+                             ["UNKNOWN_INCHI_KEY", 3]],
+                            columns=["standard_inchi_key", "index_column"])
+
+    def test_transform(self):
+        X_transformed = self.transformer.transform(self.get_X())
+        expected_df = pd.DataFrame(
+            [['OWRSAHYFSSNENM-UHFFFAOYSA-N', 0,
+              'InChI=1S/C17H12ClN3O3/c1-10-8-11(21-17(24)20-15(22)9-19-21)6-7-12(10)16(23)13-4-2-3-5-14(13)18/h2-9H,1H3,(H,20,22,24)'],
+             ['YOMWDCALSDWFSV-UHFFFAOYSA-N', 1,
+              'InChI=1S/C18H16ClN3O3/c1-10-7-14(22-18(25)21-15(23)9-20-22)8-11(2)16(10)17(24)12-3-5-13(19)6-4-12/h3-9,17,24H,1-2H3,(H,21,23,25)'],
+             ['OWRSAHYFSSNENM-UHFFFAOYSA-N', 2,
+              'InChI=1S/C17H12ClN3O3/c1-10-8-11(21-17(24)20-15(22)9-19-21)6-7-12(10)16(23)13-4-2-3-5-14(13)18/h2-9H,1H3,(H,20,22,24)']],
+            columns=['standard_inchi_key', 'index_column', 'standard_inchi'])
         X_transformed = X_transformed.sort_values('index_column')
         X_transformed.index = list(range(len(X_transformed)))
         pd.testing.assert_frame_equal(X_transformed, expected_df)
@@ -44,7 +87,8 @@ class TestSequenceLoader(unittest.TestCase):
 class TestColumnFilter(unittest.TestCase):
     transformer = ColumnFilter(colnames=['sequence', 'compound_id'])
 
-    def get_X(self):
+    @staticmethod
+    def get_X():
         return pd.DataFrame([["ACGTG", 0, "Q9Y4K4"],
                              ["GGTGACG", 1, "XRFTG4"]],
                             columns=["sequence", "compound_id", "uniprot_id"])
