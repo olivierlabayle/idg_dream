39dcce3f89682780d613c3859c4579c176256485
Olivier Labayle <olabayle@gmail.com>
2019-03-16 14:42:15 +0100

    add data analysis

--- bin/create_training_set.py
+++ bin/create_training_set.py
@@ -21,8 +21,12 @@ def drop_training_set(engine):
     engine.execute("DROP TABLE IF EXISTS training_set;")
 
 
+def drop_dtc_table(engine):
+    engine.execute("DROP TABLE IF EXISTS dtc_table;")
+
+
 def load_training_data(path, chunk_size):
-    for chunk in  pd.read_csv(path, sep=",", header=0, chunksize=chunk_size):
+    for chunk in pd.read_csv(path, sep=",", header=0, chunksize=chunk_size):
         yield chunk
 
 
@@ -43,6 +47,16 @@ def create_training_set(db_port, data_path, chunk_size):
             processed_chunk.to_sql("training_set", con=engine, index=False, if_exists='append')
 
 
+def create_dtc_table(db_port, data_path, chunk_size):
+    engine = create_engine(f'postgresql+pg8000://idg_dream@127.0.0.1:{db_port}/idg_dream', echo=False)
+    connection = engine.connect()
+    drop_dtc_table(engine)
+    for chunk in load_training_data(data_path, chunk_size=chunk_size):
+        print(f"Inserting new chunk of size :{len(chunk)}.")
+        chunk.to_sql("dtc_table", con=connection, index=False, if_exists='append')
+    connection.close()
+
+
 if __name__ == '__main__':
     args = docopt.docopt(__doc__)
-    create_training_set(args['--db-port'], args['DATA_PATH'], int(float(args["--chunk-size"])))
+    create_dtc_table(args['--db-port'], args['DATA_PATH'], int(float(args["--chunk-size"])))
