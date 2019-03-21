a70c0bc0a690df1428015f032c1492dd14f30bd8
olabayle <olivier.labayle@abolis.fr>
2019-03-21 18:46:43 +0100

    correct sparse linear layer

--- idg_dream/models.py
+++ idg_dream/models.py
@@ -1,10 +1,26 @@
 import torch
+import math
 import torch.nn as nn
 
 
-class SparseLinear(nn.Linear):
+class SparseLinear(nn.Module):
+    def __init__(self, in_features, out_features):
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = nn.Parameter(torch.Tensor(in_features, out_features))
+        self.bias = nn.Parameter(torch.Tensor(out_features))
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+        bound = 1 / math.sqrt(fan_in)
+        nn.init.uniform_(self.bias, -bound, bound)
+
     def forward(self, input):
-        return torch.sparse.mm(input, self.weight.t()) + self.bias
+        return torch.sparse.mm(input, self.weight) + self.bias
 
 
 class Baseline(nn.Module):
@@ -19,15 +35,24 @@ class Baseline(nn.Module):
         self.num_kmers = num_kmers
         self.num_fingerprints = num_fingerprints
         self.embedding_dim = embedding_dim
-        self.protein_branch = nn.Sequential(SparseLinear(num_kmers, self.embedding_dim), nn.ReLU())
-        self.compound_branch = nn.Sequential(SparseLinear(num_fingerprints, self.embedding_dim), nn.ReLU())
-        self.output_branch = nn.Sequential(
-            nn.Linear(2 * self.embedding_dim, self.embedding_dim), nn.ReLU(),
-            nn.Linear(self.embedding_dim, 1)
-        )
+        self.relu = nn.ReLU()
+        self.protein_linear = SparseLinear(num_kmers, self.embedding_dim)
+        self.compound_linear = SparseLinear(num_fingerprints, self.embedding_dim)
+        self.joined_linear = nn.Linear(2 * self.embedding_dim, self.embedding_dim)
+        self.out_linear = nn.Linear(self.embedding_dim, 1)
+        # self.output_branch = nn.Sequential(
+        #     nn.Linear(2 * self.embedding_dim, self.embedding_dim),
+        #     nn.ReLU(),
+        #     nn.Linear(self.embedding_dim, 1)
+        # )
 
     def forward(self, protein_input, compound_input):
-        protein_embedding = self.protein_branch(protein_input)
-        compound_embedding = self.compound_branch(compound_input)
+        protein_embedding = self.protein_linear(protein_input)
+        protein_embedding = self.relu(protein_embedding)
+        compound_embedding = self.compound_linear(compound_input)
+        compound_embedding = self.relu(compound_embedding)
         joined = torch.cat((protein_embedding, compound_embedding), dim=1)
-        return self.output_branch(joined)
+        joined = self.joined_linear(joined)
+        joined = self.relu(joined)
+        out = self.out_linear(joined)
+        return out
--- tests/tests_idg_dream/tests_models.py
+++ tests/tests_idg_dream/tests_models.py
@@ -1,27 +1,60 @@
 import unittest
 import torch
-import numpy as np
+from torch import optim
+from torch.nn import MSELoss
 
 from idg_dream.models import Baseline, SparseLinear
+
+
+def sparse_input():
+    # Let's define the following 2 lines sparse tensor [[0, 1, 0, 0],
+    #                                                   [0, 0, 1, 1]]
+    indexes = torch.LongTensor([[0, 1, 1],
+                                [1, 2, 3]])
+    values = torch.FloatTensor([1, 1, 1])
+    return torch.sparse.FloatTensor(indexes, values, torch.Size([2, 4]))
 
 
 class TestBaseline(unittest.TestCase):
     def setUp(self):
         torch.manual_seed(0)
-        self.model = Baseline(num_kmers=5, num_fingerprints=6, embedding_dim=7)
+        self.model = Baseline(num_kmers=4, num_fingerprints=4, embedding_dim=6)
 
     def test_forward(self):
-        protein_input = torch.LongTensor([[1, 3, 0, 3, 4],
-                                          [3, 0, 0, 2, 1]])
-        compound_input = torch.LongTensor([[5, 2, 4],
-                                           [4, 2, 1]])
-        scores = self.model(protein_input, compound_input)
-        np.testing.assert_allclose(
-            scores.detach(),
-            torch.tensor([[0.3311],
-                          [0.3179]]),
+        protein_input = sparse_input()
+        compound_input = sparse_input()
+        out = self.model(protein_input, compound_input)
+        self.assertTrue(torch.allclose(
+            out,
+            torch.FloatTensor([[-0.2538],
+                               [-0.2858]]),
             rtol=1e-4
-        )
+        ))
+
+    def test_backward(self):
+        optimizer = optim.SGD(self.model.parameters(), lr=0.1)
+        loss_fn = MSELoss()
+        target = torch.FloatTensor([[1e-5],
+                                    [3.6e-4]])
+
+        protein_input = sparse_input()
+        compound_input = sparse_input()
+        old_params = [p.clone().detach() for p in self.model.parameters()]
+
+        out = self.model(protein_input, compound_input)
+        loss = loss_fn(out, target)
+        loss.backward()
+
+        for param in self.model.parameters():
+            self.assertIsNotNone(param.grad)
+
+        optimizer.step()
+
+        for i, param in enumerate(self.model.parameters()):
+            self.assertFalse(torch.allclose(
+                param.data,
+                old_params[i]
+            ))
 
 
 class TestSparseLinear(unittest.TestCase):
@@ -30,19 +63,16 @@ class TestSparseLinear(unittest.TestCase):
         self.model = SparseLinear(4, 6)
 
     def test_forward(self):
-        # Let's define the following 2 lines sparse tensor [[0, 1, 0, 0],
-        #                                                   [0, 0, 1, 1]]
-        indexes = torch.LongTensor([[0, 1, 1],
-                                    [1, 2, 3]])
-        values = torch.FloatTensor([1, 1, 1])
-        input = torch.sparse.FloatTensor(indexes, values, torch.Size([2, 4]))
+        input = sparse_input()
         output = self.model(input)
+
         # The first line is defined by
-        first_line = self.model.weight.t()[1, :] + self.model.bias
+        first_line = self.model.weight[1, :] + self.model.bias
         # The second line is defined by
-        second_line = self.model.weight.t()[2, :] + self.model.weight.t()[3, :] + self.model.bias
+        second_line = self.model.weight[2, :] + self.model.weight[3, :] + self.model.bias
         # expected output is the concatenation of the 2 lines
         expected_output = torch.stack((first_line, second_line))
+
         self.assertTrue(torch.all(torch.eq(
             expected_output,
             output
