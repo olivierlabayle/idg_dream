77c80905c72326fcdce6780b434255da24ac8f84
olabayle <olivier.labayle@abolis.fr>
2019-03-22 11:44:38 +0100

    add some documentation

--- idg_dream/pipelines.py
+++ idg_dream/pipelines.py
@@ -7,8 +7,24 @@ from functools import partial
 from idg_dream.utils import collate_to_sparse_tensors
 
 
-def baseline(engine, kmer_size=3, radius=2, ecfp_dim=2 ** 10, embedding_dim=10, lr=0.1, max_epochs=5,
+def baseline(engine=None, kmer_size=3, radius=2, ecfp_dim=2 ** 10, embedding_dim=10, lr=0.1, max_epochs=5,
              device='cpu', loaders=False, train_split=None):
+    """
+    This pipeline is a neural net baseline using sparsed input fingerprints for both the compound (ecfp) and the
+    enzyme (k-mers).
+    :param engine: If loaders is true, the you should also specify an engine
+    :param kmer_size: The k-mer size used for the enzyme's descriptor
+    :param radius: The radius used in ecfp
+    :param ecfp_dim: The dimension of the byte space used by ecfp algorithm
+    :param embedding_dim: Both enzyme and compounds are embedded in the neural net in a space of the same size
+    :param lr: the neural net base learning rate
+    :param max_epochs: Maximum number of epochs to run
+    :param device: The device on which computation will take place
+    :param loaders: If the sequence of the enzyme and the inchi of the compound is not present in the base dataset,
+                    use the idg_dream database to load them, the engine argument should then be specified
+    :param train_split: if None, no internal cross validation is made, else a skorch CVSplit object
+    :return: sklearn.pipeline.Pipeline
+    """
     protein_encoder = ProteinEncoder(kmer_size=kmer_size)
     num_kmers = len(protein_encoder.kmers_mapping)
     collate_fn = partial(collate_to_sparse_tensors, protein_input_size=num_kmers, compound_input_size=ecfp_dim)
---
+++ tests/tests_idg_dream/tests_pipelines.py
@@ -0,0 +1,45 @@
+import unittest
+import torch
+import os
+
+from skorch.dataset import CVSplit
+
+from idg_dream import pipelines
+from idg_dream.utils import get_engine, load_from_csv
+from idg_dream.settings.test import DB_PORT
+
+
+class TestBaselinePipeline(unittest.TestCase):
+    engine = get_engine(db_port=DB_PORT)
+
+    def setUp(self):
+        torch.manual_seed(0)
+        self.pipeline = pipelines.baseline(engine=self.engine, max_epochs=10)
+        self.training_sample_path = os.path.join("tests", "training_sample_100.csv")
+
+    def test_fit(self):
+        X, y = load_from_csv(self.training_sample_path)
+        self.pipeline.fit(X, y)
+        train_losses = [(h['epoch'], h['train_loss']) for h in self.pipeline._final_estimator.history]
+        self.assertEqual(
+            train_losses,
+            [(1, 0.09648986905813217), (2, 0.03842122480273247), (3, 0.03089130111038685),
+             (4, 0.025661496445536613), (5, 0.021845480427145958), (6, 0.01892860420048237),
+             (7, 0.016651982441544533), (8, 0.014866072684526443), (9, 0.013424623757600784),
+             (10, 0.012224127538502216)]
+        )
+
+    def test_cross_validate(self):
+        self.pipeline = pipelines.baseline(engine=self.engine, max_epochs=10, train_split=CVSplit(0.2, random_state=0))
+        X, y = load_from_csv(self.training_sample_path)
+        self.pipeline.fit(X, y)
+        losses = [(h['epoch'], h['train_loss'], h['valid_loss']) for h in self.pipeline._final_estimator.history]
+        self.assertEqual(
+            losses,
+            [(1, 0.10234718769788742, 0.02837774157524109), (2, 0.0399295836687088, 0.024141712114214897),
+             (3, 0.031169436872005463, 0.021233337000012398), (4, 0.025440266355872154, 0.01903749629855156),
+             (5, 0.021385645493865013, 0.017291951924562454), (6, 0.01837981678545475, 0.015881769359111786),
+             (7, 0.016166657209396362, 0.014710157178342342), (8, 0.0143850427120924, 0.013715007342398167),
+             (9, 0.012921981513500214, 0.012865738943219185), (10, 0.011702113784849644, 0.012164301238954067)]
+
+        )
