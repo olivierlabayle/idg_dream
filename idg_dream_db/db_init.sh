88fbca17090fbe8beaa7067bb5b3aa1def18fb2d
olabayle <olivier.labayle@abolis.fr>
2019-03-22 11:52:06 +0100

    update docker compose idg_dream

--- idg_dream_db/db_init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER idg_dream;
-    CREATE DATABASE idg_dream;
-    GRANT ALL PRIVILEGES ON DATABASE idg_dream TO idg_dream;
-EOSQL
