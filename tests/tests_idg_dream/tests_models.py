0b6b615cb0f606e42ce0d74fea475d6295d99660
olabayle <olivier.labayle@abolis.fr>
2019-04-02 16:24:50 +0200

    add test for LSTMFingerprint network

--- tests/tests_idg_dream/tests_models.py
+++ tests/tests_idg_dream/tests_models.py
@@ -16,6 +16,13 @@ def sparse_input():
     return torch.sparse.FloatTensor(indexes, values, torch.Size([2, 4]))
 
 
+def protein_inputs():
+    protein_input = torch.tensor([[3, 4, 1, 2, 5, 0],
+                                  [2, 0, 0, 6, 6, 6]])
+    protein_lengths = torch.LongTensor([6, 3])
+    return protein_input, protein_lengths
+
+
 class TestBaseline(unittest.TestCase):
     def setUp(self):
         torch.manual_seed(0)
@@ -85,17 +92,11 @@ class TestBiLSTMProteinEmbedder(unittest.TestCase):
         torch.manual_seed(0)
         self.model = BiLSTMProteinEmbedder(num_kmers=7, embedding_dim=10, hidden_size=5, mlp_sizes=(9,))
 
-    def get_inputs(self):
-        protein_input = torch.tensor([[3, 4, 1, 2, 5, 0],
-                                      [2, 0, 0, 6, 6, 6]])
-        protein_lengths = torch.LongTensor([6, 3])
-        return protein_input, protein_lengths
-
     def test_forward(self):
-        out = self.model(*self.get_inputs())
+        out = self.model(*protein_inputs())
         self.assertTrue(torch.allclose(
             torch.tensor([[0.0833, 0.0000, 0.1195, 0.0000, 0.2055, 0.2779, 0.0000, 0.0000, 0.4271],
-                [0.0787, 0.0000, 0.1505, 0.0000, 0.1646, 0.2946, 0.0481, 0.0000, 0.3201]]),
+                          [0.0787, 0.0000, 0.1505, 0.0000, 0.1646, 0.2946, 0.0481, 0.0000, 0.3201]]),
             out,
             atol=1e-4
         ))
@@ -109,7 +110,7 @@ class TestBiLSTMProteinEmbedder(unittest.TestCase):
 
         old_params = [p.clone().detach() for p in model.parameters()]
 
-        out = model(*self.get_inputs())
+        out = model(*protein_inputs())
         loss = loss_fn(out, target)
         loss.backward()
 
@@ -140,3 +141,46 @@ class TestGCNLayer(unittest.TestCase):
                           [0.0104, -0.0371, 0.3398, -0.4799]]),
             atol=1e-4
         ))
+
+
+class TestSiameseBiLSTMFingerprints(unittest.TestCase):
+    def setUp(self):
+        torch.manual_seed(0)
+        self.model = SiameseBiLSTMFingerprints(num_kmers=7, num_fingerprints=4, embedding_dim=10, hidden_size=5,
+                                               mlp_sizes=(9,))
+
+    def test_forward(self):
+        protein_input, protein_lengths = protein_inputs()
+        compound_input = sparse_input()
+        out = self.model(protein_input, compound_input, protein_lengths)
+        self.assertTrue(torch.allclose(
+            out,
+            torch.tensor([[-0.0220],
+                          [-0.0351]]),
+            atol=1e-4
+        ))
+
+    def test_backward(self):
+        optimizer = optim.SGD(self.model.parameters(), lr=1)
+        loss_fn = MSELoss()
+        target = torch.FloatTensor([[1e-5],
+                                    [3.6e-4]])
+
+        old_params = [p.clone().detach() for p in self.model.parameters()]
+
+        protein_input, protein_lengths = protein_inputs()
+        compound_input = sparse_input()
+        out = self.model(protein_input, compound_input, protein_lengths)
+        loss = loss_fn(out, target)
+        loss.backward()
+
+        for param in self.model.parameters():
+            self.assertIsNotNone(param.grad)
+
+        optimizer.step()
+
+        for i, param in enumerate(self.model.parameters()):
+            self.assertFalse(torch.allclose(
+                param.data,
+                old_params[i]
+            ))
